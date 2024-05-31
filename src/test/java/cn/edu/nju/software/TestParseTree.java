33b8242e86c9e12bf72abfabaa74b743ec54d955
ymhui <221870120@smail.nju.edu.cn>
2024-05-31 21:13:54 +0800

    feat: add simple test in package ir

--- src/test/java/cn/edu/nju/software/TestParseTree.java
+++ src/test/java/cn/edu/nju/software/TestParseTree.java
@@ -1,14 +1,14 @@
 package test.java.cn.edu.nju.software;
 import main.java.cn.edu.nju.software.Main;
 
-//import org.junit.Test;
-//
-//public class TestParseTree {
-//    private static final String PREFIX_SY = "src/test/resources/sy";
-//    @Test
-//    public void test() {
-//        Main.main(PREFIX_SY + "add.sy",
-//                "-o", "output/add.s",
-//                "--emit-llvm", "-O0");
-//    }
-//}
+import org.junit.Test;
+
+public class TestParseTree {
+    private static final String PREFIX_SY = "src/test/resources/sy";
+    @Test
+    public void test() {
+        Main.main(PREFIX_SY + "add.sy",
+                "-o", "output/add.s",
+                "--emit-llvm", "-O0");
+    }
+}
