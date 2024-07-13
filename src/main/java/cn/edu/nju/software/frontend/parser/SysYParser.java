500e76a4e0e3038b5a10f6c50021f0a38b49e1e8
chaos-warzh <haidedeyu@163.com>
2024-07-13 13:59:01 +0800

    fix: SysYLexer bugs

--- src/main/java/cn/edu/nju/software/frontend/parser/SysYParser.java
+++ src/main/java/cn/edu/nju/software/frontend/parser/SysYParser.java
@@ -23,10 +23,8 @@ public class SysYParser extends Parser {
 		NEQ=18, LT=19, GT=20, LE=21, GE=22, NOT=23, AND=24, OR=25, L_PAREN=26, 
 		R_PAREN=27, L_BRACE=28, R_BRACE=29, L_BRACKT=30, R_BRACKT=31, COMMA=32, 
 		SEMICOLON=33, IDENT=34, INTEGER_CONST=35, FLOAT_CONST=36, Decimal_floating_constant=37, 
-		Hexadecimal_floating_constant=38, Fractional_constant=39, Sign=40, Digit_sequence=41, 
-		Hexadecimal_fractional_constant=42, Binary_exponent_part=43, Hexadecimal_digit_sequence=44, 
-		Floating_suffix=45, Hexadecimal_prefix=46, Hexadecimal_digit=47, WS=48, 
-		LINE_COMMENT=49, MULTILINE_COMMENT=50;
+		Hexadecimal_floating_constant=38, Fractional_constant=39, WS=40, LINE_COMMENT=41, 
+		MULTILINE_COMMENT=42;
 	public static final int
 		RULE_program = 0, RULE_funcUse = 1, RULE_funcName = 2, RULE_compUnit = 3, 
 		RULE_decl = 4, RULE_constDecl = 5, RULE_bType = 6, RULE_constDef = 7, 
@@ -63,9 +61,7 @@ public class SysYParser extends Parser {
 			"EQ", "NEQ", "LT", "GT", "LE", "GE", "NOT", "AND", "OR", "L_PAREN", "R_PAREN", 
 			"L_BRACE", "R_BRACE", "L_BRACKT", "R_BRACKT", "COMMA", "SEMICOLON", "IDENT", 
 			"INTEGER_CONST", "FLOAT_CONST", "Decimal_floating_constant", "Hexadecimal_floating_constant", 
-			"Fractional_constant", "Sign", "Digit_sequence", "Hexadecimal_fractional_constant", 
-			"Binary_exponent_part", "Hexadecimal_digit_sequence", "Floating_suffix", 
-			"Hexadecimal_prefix", "Hexadecimal_digit", "WS", "LINE_COMMENT", "MULTILINE_COMMENT"
+			"Fractional_constant", "WS", "LINE_COMMENT", "MULTILINE_COMMENT"
 		};
 	}
 	private static final String[] _SYMBOLIC_NAMES = makeSymbolicNames();
@@ -2484,9 +2480,9 @@ public class SysYParser extends Parser {
 	}
 
 	public static final String _serializedATN =
-		"\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\3\64\u0152\4\2\t\2"+
-		"\4\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7\t\7\4\b\t\b\4\t\t\t\4\n\t\n\4\13"+
-		"\t\13\4\f\t\f\4\r\t\r\4\16\t\16\4\17\t\17\4\20\t\20\4\21\t\21\4\22\t\22"+
+		"\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\3,\u0152\4\2\t\2\4"+
+		"\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7\t\7\4\b\t\b\4\t\t\t\4\n\t\n\4\13\t"+
+		"\13\4\f\t\f\4\r\t\r\4\16\t\16\4\17\t\17\4\20\t\20\4\21\t\21\4\22\t\22"+
 		"\4\23\t\23\4\24\t\24\4\25\t\25\4\26\t\26\4\27\t\27\4\30\t\30\4\31\t\31"+
 		"\4\32\t\32\4\33\t\33\4\34\t\34\4\35\t\35\4\36\t\36\4\37\t\37\3\2\3\2\3"+
 		"\3\3\3\3\3\5\3D\n\3\3\3\3\3\3\4\3\4\3\5\3\5\6\5L\n\5\r\5\16\5M\3\5\3\5"+
