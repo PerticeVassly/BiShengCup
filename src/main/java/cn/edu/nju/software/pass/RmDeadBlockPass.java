8870a27dd8d84741b115bc8c02884c1c95ab7105
ymhui <221870120@smail.nju.edu.cn>
2024-07-26 20:50:01 +0800

    rm dead blocks frame

---
+++ src/main/java/cn/edu/nju/software/pass/RmDeadBlockPass.java
@@ -0,0 +1,22 @@
+package cn.edu.nju.software.pass;
+
+import cn.edu.nju.software.ir.module.ModuleRef;
+
+public class RmDeadBlockPass {
+    private final ModuleRef module;
+    private final EliminateConstExp eliminateConstExp;
+
+    public RmDeadBlockPass(ModuleRef module) {
+        this.module = module;
+        eliminateConstExp = new EliminateConstExp(module);
+    }
+
+    private void rmDeadBlocks() {
+        // TODO
+    }
+
+    public void runOnModule() {
+        rmDeadBlocks();
+        eliminateConstExp.runOnModule();
+    }
+}
