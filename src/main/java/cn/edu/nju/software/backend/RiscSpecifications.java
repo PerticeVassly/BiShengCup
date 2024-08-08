e3ba02799096a5c0cf021698f1ce481250c85cd4
Chenyang Wang <1546195620@qq.com>
2024-08-08 19:29:49 +0800

    回退寄存器使用

--- src/main/java/cn/edu/nju/software/backend/RiscSpecifications.java
+++ src/main/java/cn/edu/nju/software/backend/RiscSpecifications.java
@@ -17,13 +17,13 @@ public record RiscSpecifications() {
     private static final String[] calleeSavedRegs = new String[] {};
     private static final String[] argRegs = new String[] {"a0", "a1", "a2", "a3", "a4", "a5", "a6", "a7"};
     private static final String[] fArgRegs = new String[] {"fa0", "fa1", "fa2", "fa3", "fa4", "fa5", "fa6", "fa7"};
-    private static final String[] tempVarRegs = new String[] {"s0","s1","fs0","fs1"};
+    private static final String[] tempVarRegs = new String[] {"s0","s1","s2","s3","fs0","fs1","fs2", "fs3"};
     private static final String[] intLocalVarRegs = new String[] {
-            "s2","s3","s4","s5","s7","s8","s9","s10","s11"
+            "s4","s5","s7","s8","s9","s10","s11"
     };
 
     private static final String[] floatLocalVarRegs = new String[] {
-            "fs2", "fs3", "fs4", "fs5", "fs6", "fs7", "fs8", "fs9", "fs10", "fs11"
+            "fs4", "fs5", "fs6", "fs7", "fs8", "fs9", "fs10", "fs11"
     };
     private static final boolean isDebug = true;
 
