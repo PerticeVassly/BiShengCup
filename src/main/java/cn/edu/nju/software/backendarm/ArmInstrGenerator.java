a6126a254a069a1c74fac98846607cde1fb62c0e
sunyiqiu <1607109698@qq.com>
2024-08-09 16:17:32 +0800

    build: 98pass

--- src/main/java/cn/edu/nju/software/backendarm/ArmInstrGenerator.java
+++ src/main/java/cn/edu/nju/software/backendarm/ArmInstrGenerator.java
@@ -202,7 +202,7 @@ public class ArmInstrGenerator implements InstructionVisitor {
     public void visit(Mod mod) {
         insertComment("mod " + mod.getLVal().getName() + " " + mod.getOperand(0).getName() + " " + mod.getOperand(1).getName());
         List<String> regs = beforeABinaryInstr(mod);
-        armInstructions.add(new ArmUdiv(new ArmRegister("r7"), new ArmRegister(regs.get(0)), new ArmRegister(regs.get(1))));
+        armInstructions.add(new ArmSdiv(new ArmRegister("r7"), new ArmRegister(regs.get(0)), new ArmRegister(regs.get(1))));
         armInstructions.add(new ArmMul(new ArmRegister("r7"), new ArmRegister("r7"), new ArmRegister(regs.get(1))));
         armInstructions.add(new ArmSub(new ArmRegister("r4"), new ArmRegister(regs.get(0)), new ArmRegister("r7")));
         afterAnInstr(mod);
