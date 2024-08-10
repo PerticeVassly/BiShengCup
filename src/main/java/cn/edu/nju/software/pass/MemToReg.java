d3c3fe1be6d45ea1e6b44545cea7ca4414bfa6f5
ymhui <221870120@smail.nju.edu.cn>
2024-08-10 10:09:03 +0000

    Update MemToReg.java

--- src/main/java/cn/edu/nju/software/pass/MemToReg.java
+++ src/main/java/cn/edu/nju/software/pass/MemToReg.java
@@ -240,7 +240,7 @@ public class MemToReg implements ModulePass {
             changed |= eliminateConstExp.runOnModule(this.module);
         }
         // reduce phi and drop phi's dead pred
-        runPhiModifyPass();
+        // runPhiModifyPass();
         modifyPhiOnModule();
         return false;
     }
